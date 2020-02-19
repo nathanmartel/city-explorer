959d2019f33d78ab85e9f0f9caf978404b39704e
Nathan Martel <nathanmartel@gmail.com>
2020-02-19 11:22:33 -0800

    Added weather

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
 const app = express();
 const request = require('superagent');
+const cors = require('cors');
 
 const geoData = require('./data/geo.json');
 const weatherData = require('./data/darksky.json');
 
 
+// Globalized for use in other functions/routes.
+let lat;
+let lng;
+
+
+app.use(cors());
+
+app.get('/', (req, res) => { res.send('Welome!'); });
+
 app.get('/location', (req, res) => {
+
+    // i.e. www.google.com/?search=abc
+    const location = request.query.search;
 
     // I assume this is where we'd process the given location from params, format appropriately, and call some function to provide lat and long?
 
-    // res.json({
-    //     formatted_query: 'Seattle, WA, USA',
-    //     latitude: '47.606210',
-    //     longitude: '-122.332071'
-    // });
-
     // const reqLocation = req.params.locationParam;
     const filteredResult = geoData.results[0];
-
-        
+    lat = filteredResult.geometry.location.lat;
+    lng = filteredResult.geometry.location.lng;
+    
     res.json({
         formatted_query: filteredResult.formatted_address,
         latitude: filteredResult.geometry.location.lat,
@@ -27,20 +35,21 @@ app.get('/location', (req, res) => {
     });
 });
 
+const getWeatherData = (lat, lng) => {
+    return weatherData.daily.data.map(forecast => {
+        return {
+            forecast: forecast.summary,
+            // Hot tip: Don't use Date, use moment library
+            time: new Date(forecast.time * 1000),            
+        };
+    });
+};
+
 app.get('/weather', (req, res) => {
 
-    // I assume this is where we'd process the given location from params, format appropriately, and call some function to provide lat and long?
+    const filteredResult = getWeatherData(lat, lng);
 
-    res.json([
-        {
-            forecast: 'Partly cloudy until afternoon.',
-            time: 'Mon Jan 01 2001'
-        },
-        {
-            forecast: 'Mostly cloudy in the morning.',
-            time: 'Tue Jan 02 2001'
-        }
-    ]);
+    res.json(filteredResult);
 });
 
 app.get('*', (req, res) => {
