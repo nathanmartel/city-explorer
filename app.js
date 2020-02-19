467bebc12c4b2bdffcfcfc6b3174ab3a271dcc10
Nathan Martel <nathanmartel@gmail.com>
2020-02-19 09:56:53 -0800

    Importing local JSON and first test written

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const express = require('express');
-
 const app = express();
+const request = require('superagent');
 
-app.get('/blah', (req, res) => {
+const geoData = require('./data/geo.json');
+const weatherData = require('./data/darksky.json');
+
+
+app.get('/location', (req, res) => {
+
+    // I assume this is where we'd process the given location from params, format appropriately, and call some function to provide lat and long?
+
+    // res.json({
+    //     formatted_query: 'Seattle, WA, USA',
+    //     latitude: '47.606210',
+    //     longitude: '-122.332071'
+    // });
+
+    // const reqLocation = req.params.locationParam;
+    const filteredResult = geoData.results[0];
+
+        
     res.json({
-        some: 'json',
+        formatted_query: filteredResult.formatted_address,
+        latitude: filteredResult.geometry.location.lat,
+        longitude: filteredResult.geometry.location.lng,
     });
+});
+
+app.get('/weather', (req, res) => {
+
+    // I assume this is where we'd process the given location from params, format appropriately, and call some function to provide lat and long?
+
+    res.json([
+        {
+            forecast: 'Partly cloudy until afternoon.',
+            time: 'Mon Jan 01 2001'
+        },
+        {
+            forecast: 'Mostly cloudy in the morning.',
+            time: 'Tue Jan 02 2001'
+        }
+    ]);
 });
 
 app.get('*', (req, res) => {
-    res.json({
-        ohno: '404 not found',
-    });
+    res.send('404 not found');
 });
 
-app.listen(3000, () => { console.log('running...'); });
+module.exports = { 
+    app: app, 
+};
